eb06a674155275ec822622e45285da1bcf65c36f
PritiShashikantGaikwad <pritigaikwad0005@gmail.com>
2025-06-20 18:01:02 +0530

    Add files via upload

---
+++ Python_Assignment21/Assignment21_3.py
@@ -0,0 +1,48 @@
+import psutil
+import os
+import time
+import sys
+def CreateLog(FolderName,Data):
+    if not os.path.exists(FolderName):
+        os.mkdir(FolderName)
+    
+    timestamp=time.ctime()
+    timestamp=timestamp.replace(":","_")
+    timestamp=timestamp.replace(" ","_")
+    timestamp=timestamp.replace("/",":")
+
+    FileName=os.path.join(FolderName,"Marvellous%s.log"%(timestamp))
+    Border="*"*80
+    fobj=open(FileName,"w")
+    fobj.write(Border+"\n")
+    fobj.write("This is automation Script ")
+    fobj.write("This file is created at:"+str(time.ctime())+"\n")
+    fobj.write(Border+"\n")
+    for value in Data:
+            fobj.write(str (value)+"\n")
+            
+    fobj.write(Border+"\n")
+    fobj.close()
+
+
+def ProcessDetails():
+
+    listprocess=[]
+
+    for proc in psutil.process_iter():
+        data=proc.as_dict(attrs=['pid','name','username'])
+        listprocess.append(data)
+
+    return listprocess
+def main():
+
+    
+           Arr=ProcessDetails()
+           CreateLog("MarvellousLogProcess",Arr)
+
+          
+   
+     
+
+if __name__=="__main__":
+    main()
